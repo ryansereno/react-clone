e5c5b08f568e64f44ab02e97a9f62346a9efb821
R <ryansereno@protonmail.com>
2022-09-02 13:43:56 -0400

    added state update

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const React = (function () {
+  let _val;
   function useState(initVal) {
-    let _val = initVal;
-    const state = () => _val;
+    const state = _val || initVal;
 
     const setState = (newVal) => {
       _val = newVal;
     };
     return [state, setState];
   }
-    return {useState}
+  function render(Component) {
+    const C = Component();
+    C.render();
+    return C;
+  }
+  return { useState, render };
 })();
 
-const [count, setCount] = React.useState(1);
-console.log(count());
-setCount(2);
-console.log(count());
+function Component() {
+  const [count, setCount] = React.useState(1);
+  return {
+    render: () => console.log(count),
+    click: () => setCount(count + 1),
+  };
+}
+
+var App = React.render(Component);
+App.click();
+var App = React.render(Component);
+App.click();
+var App = React.render(Component);
+App.click();
