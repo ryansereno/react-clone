c4f352326a72f3b95fb3b34b5d68e14be0cf1940
R <ryansereno@protonmail.com>
2022-09-02 13:56:14 -0400

    added multiple hook handling

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 const React = (function () {
-  let _val;
+  let hooks = [];
+  let i = 0;
   function useState(initVal) {
-    const state = _val || initVal;
-
+    const state = hooks[i] || initVal;
+    const _i = i
     const setState = (newVal) => {
-      _val = newVal;
+      hooks[_i] = newVal;
     };
+    i++;
     return [state, setState];
   }
+
   function render(Component) {
+      i =0
     const C = Component();
     C.render();
     return C;
   }
+
   return { useState, render };
 })();
 
 function Component() {
   const [count, setCount] = React.useState(1);
+  const [text, setText] = React.useState("apple");
+
   return {
-    render: () => console.log(count),
+    render: () => console.log({ count, text }),
     click: () => setCount(count + 1),
+    typeText: (word) => setText(word),
   };
 }
 
 var App = React.render(Component);
 App.click();
+App.typeText("watermelon");
 var App = React.render(Component);
 App.click();
 var App = React.render(Component);
