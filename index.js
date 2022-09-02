dd59b9efc472e134b8f0520f0a6a25647c1338b1
R <ryansereno@protonmail.com>
2022-09-02 12:33:18 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const React = (function () {
+  function useState(initVal) {
+    let _val = initVal;
+    const state = () => _val;
+
+    const setState = (newVal) => {
+      _val = newVal;
+    };
+    return [state, setState];
+  }
+    return {useState}
+})();
+
+const [count, setCount] = React.useState(1);
+console.log(count());
+setCount(2);
+console.log(count());
