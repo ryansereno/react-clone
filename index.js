a1c08505b0b9cb4a1f36e96932e015b0f6b2dffe
R <ryansereno@protonmail.com>
2022-09-02 16:27:51 -0400

    added useEffect hook

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const React = (function () {
   let hooks = [];
   let i = 0;
+
   function useState(initVal) {
     const state = hooks[i] || initVal;
     const _i = i
@@ -15,27 +16,53 @@ const React = (function () {
       i =0
     const C = Component();
     C.render();
+      console.log("hook array= ", hooks)
     return C;
   }
+    function useEffect(callback, dependencyArray){
+        const oldDependencies = hooks[i]
+        let hasChanged = true
+        if (oldDependencies){
+            hasChanged = dependencyArray.some(
+                (dep, indx) => !Object.is(dep, oldDependencies[indx])
+            )
+        }
+        if (hasChanged){
+            callback()
+        }
+        hooks[i] = dependencyArray
+        i++
+    }
 
-  return { useState, render };
+  return { useState, render, useEffect };
 })();
 
 function Component() {
-  const [count, setCount] = React.useState(1);
   const [text, setText] = React.useState("apple");
+  const [fruit, setFruit] = React.useState("🍍");
+  const [count, setCount] = React.useState(1);
+
+    React.useEffect(() =>{
+        console.log('effect triggered')
+    }, [count,fruit])
 
   return {
-    render: () => console.log({ count, text }),
+    render: () => console.log({ count, text, fruit }),
     click: () => setCount(count + 1),
     typeText: (word) => setText(word),
+    typeFruit: (fruit) => setFruit(fruit),
   };
 }
 
 var App = React.render(Component);
-App.click();
-App.typeText("watermelon");
-var App = React.render(Component);
+//App.typeText("watermelon");
 App.click();
 var App = React.render(Component);
-App.click();
+//App.typeText("lemon");
+App.typeFruit("🍓")
+//App.click();
+var App = React.render(Component);
+//App.typeText("apppppppp");
+//var App = React.render(Component);
+//var App = React.render(Component);
+//var App = React.render(Component);
